fde52feab9b9ad640073a524a5ec3e19b74f9d12
Hugo Delbegue <hugodelbegue@gmail.com>
2023-02-21 22:31:26 +0100

    Modif vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/space-tourism-website-main/',
   plugins: [vue()],
   resolve: {
     alias: {
